8530c73b952d913b9f35903e2ed00842a0b6bfd7
Daniel Lang <langd052405@gmail.com>
2024-07-15 16:40:01 -0400

    updating setup.sh to activate virtual environment when run

--- setup.sh
+++ setup.sh
@@ -3,12 +3,11 @@
 echo "Opening VSCode"
 code . &
 
-
-echo "Activating Virtual Environment"
-source env/Scripts/activate &
-
 echo "Opening Docker Desktop"
 "C:/Program Files/Docker/Docker/frontend/Docker Desktop.exe" &&
 
 echo "Building and Spinning Up Containers"
-docker compose -f compose.nginx.yaml up -d --build
+docker compose -f compose.nginx.yaml up -d --build &&
+
+echo "Activating Virtual Environment"
+source env/Scripts/activate
